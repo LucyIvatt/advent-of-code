15ace71dd7176534e846059ea1bb4fcdacb21caa
Lucy Ivatt <lucyivatt@gmail.com>
2025-12-09 13:56:01 +0000

    docs: vague plan in a string for day 9 p2
    Signed-off-by: Lucy Ivatt <lucyivatt@gmail.com>

--- python/year_2025/day_09_movie_theatre/solution.py
+++ python/year_2025/day_09_movie_theatre/solution.py
@@ -7,17 +7,23 @@ def part_one(puzzle_input):
 
     coords = [tuple(map(int, line.split(","))) for line in puzzle_input]
     for c in combinations(coords, 2):
+        print(c)
         area = abs(c[0][0]-c[1][0]+1) * abs(c[0][1] - c[1][1]+1)
         highest_area = max(highest_area, area)
     return highest_area
 
 
 def part_two(puzzle_input):
+    # extrapolate all the edge so we have a list of all edge coordinates
+    # for all four corners of the rectangle, cast a ray out until its out of bounds,
+    #   if coord is in the edge set, add to count of boundsary crosses
+    # if it crosses the boundary odd number of times its within the shape
+    # if its even its outside
     pass
 
 
 def main():
-    puzzle_input = input_data("python/year_2025/day_09_movie_theatre/input.txt")
+    puzzle_input = input_data("python/year_2025/day_09_movie_theatre/example.txt")
 
     p1, p1_time = time_function(part_one, puzzle_input)
     p2, p2_time = time_function(part_two, puzzle_input)
